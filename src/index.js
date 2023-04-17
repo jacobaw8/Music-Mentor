87af8afb3fba88ee9f0b5723148f895c0a85c19e
jacobaw8 <jacobaw8@gmail.com>
2023-04-16 23:20:30 -0400

    Solved simple problem

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { registerRootComponent } from 'expo';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
-import BrowserRouter from 'react-router-dom/BrowserRouter';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <React.StrictMode>
