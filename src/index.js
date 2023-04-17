d8caf59f06b4b40e6af20d3bf8f4e0c66909b650
jacobaw8 <jacobaw8@gmail.com>
2023-04-16 23:44:41 -0400

    Tried adding Route

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import { registerRootComponent } from 'expo';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<App />}>
+      </Route>
+    )
+  );
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <App />
-        </BrowserRouter>
+        <RouterProvider router={router} />
     </React.StrictMode>,
     document.getElementById('root')
 );
